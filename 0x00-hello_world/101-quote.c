6f853b71fd122d14448302bdb4305d3ae47cfdb1
mermaidseaweav <eng.gameliaahmed@gmail.com>
2023-02-16 14:50:55 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A c program that prints a line to the standard error
+ * main - Entry
  * Return 1 (Success)
  */
 int main(void)
 {
-write("and that piece of art is useful" - Dora Korpar, 2015-10-19\n", stdout);
+write(1, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 59);
 return (1);
