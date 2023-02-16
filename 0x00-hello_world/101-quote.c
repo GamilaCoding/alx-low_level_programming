d5870b7620064bb31f62ccf5f21fee72ee4e8655
mermaidseaweav <eng.gameliaahmed@gmail.com>
2023-02-16 13:56:15 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
  * main - A c program that prints a line to the standard error
  * Return 1 (Success)
  */
-in main(void)
+int main(void)
 {
 write("and that piece of art is useful" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
