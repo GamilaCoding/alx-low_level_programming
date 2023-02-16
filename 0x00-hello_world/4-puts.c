3ce0568f68ae0a81d769332fc07537b72e323359
mermaidseaweav <eng.gameliaahmed@gmail.com>
2023-02-16 11:18:00 -0800

    100-intel

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-
-
 /**
- * main - prints "Programming is like building a multilingual
- * puzzle, followed by a newline."
- * Return: 0 
- */
+ * main - Entry
+ * 
+ * Return: Always 0 
+*/
+
 int main(void)
 {
         puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+
 }
