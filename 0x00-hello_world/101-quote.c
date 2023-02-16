0a6c3ae89ec3c0dba5142173c7175b75be7a1773
mermaidseaweav <eng.gameliaahmed@gmail.com>
2023-02-16 14:58:39 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry
- * Return 1 (Success)
+ * main -Entry
+ * Return: 1
  */
 int main(void)
 {
-write(1, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 59);
-return (1);
+	write(1, "and the piece of art is useful\" - "Dora Korpar, 2015-10-19\n", 59);
+	return (1);
