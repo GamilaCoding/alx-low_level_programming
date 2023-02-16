ad2c3de65f15570dd0084bbc6c97462d8489d855
mermaidseaweav <eng.gameliaahmed@gmail.com>
2023-02-16 13:44:04 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-fput(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+fputs(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
