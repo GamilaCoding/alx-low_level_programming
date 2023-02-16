c95d9c582c99466f4109cdddc405beaa62673560
mermaidseaweav <eng.gameliaahmed@gmail.com>
2023-02-16 15:07:21 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main -Entry
+ * main - Entry
  * Return: 1
  */
 int main(void)
 {
-write( "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
