391d4324497f6934251142da38ba5eabaf3bd0e5
mermaidseaweav <eng.gameliaahmed@gmail.com>
2023-02-17 07:53:46 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - main block
+ * Descriptaion: prints all signal digit number of base 10
+ * starting from 0, followed by new line 
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'z';
+	while (c >= 'a')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
