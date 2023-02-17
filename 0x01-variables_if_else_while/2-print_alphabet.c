7b67016e9d5c6452e34c8e196c43e0623c0cb562
mermaidseaweav <eng.gameliaahmed@gmail.com>
2023-02-17 10:40:14 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,17 +2,18 @@
 #include <stdlib.h>
 /**
  * main - main block
- * Description: GEt a random it with 5
+ * Description: Get a random it with 5
  * Return: 0
  */
 int main(void)
 {
 	 char c = 'a';
+
 	 while (c <= 'z')
         {
 		putchar(c);
 		c++;
-        }
+        } 
         putchar('\n');
         return (0);
 }
