5ae9b00a9a571a12d978fd05202a060743e4b7de
mermaidseaweav <eng.gameliaahmed@gmail.com>
2023-02-17 08:14:26 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - main block
+* Description:prints all signle digit number of base 10
+*starting from 0, followed by new line.
+* Return: Always 0
+*/
+int main(void)
+{
+	char ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar(ch);
+	return (0);
+}
