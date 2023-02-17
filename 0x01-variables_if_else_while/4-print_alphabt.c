ea7787bc8bb28ce44d968cc6ecb6408b7ab585b4
mermaidseaweav <eng.gameliaahmed@gmail.com>
2023-02-17 06:41:20 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alpabet in lowercase
+ * followed by a new line, expect q and e
+ * Return: Alawys 0
+ */
+int main(void)
+{
+	char la;
+
+	for (la = 'a'; la <= '2'; a++)
+	{
+		if (la != 'e' && la != 'q')
+			putchar(la);
+	}
+	putchar('\n');
+	
+	return (0);
+}
