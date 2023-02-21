7588717ecd0dd0e2f37b9e5188cb682b8a7a5a1b
mermaidseaweav <eng.gameliaahmed@gmail.com>
2023-02-21 06:05:04 -0800

    5-main.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - function that prints the sign of a number ..
+ * @n : the input number to check
+ * Return: int.
+ */
+
+int print_sign(int n)
+{
+	if (n >0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+}
