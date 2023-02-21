61ebaee87417abfa1a789960536ececc6925a278
mermaidseaweav <eng.gameliaahmed@gmail.com>
2023-02-21 02:49:13 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,14 +8,13 @@
 int main(void)
 {
 	_putchar('_');
-	_putchar("p");
-	_putchar("u");
-	_putchar("t");
-	_putchar("c");
-        _putchar("h");
-        _putchar("a");
-        _putchar("r");
+	_putchar(char "p");
+	_putchar(char "u");
+	_putchar(char "t");
+	_putchar(char "c");
+        _putchar(char "h");
+        _putchar(char "a");
+        _putchar(char "r");
         _putchar('\n');	
 	return (0);
 }
-
