dfccfb7599997c26623878a73a22d014ddbae0bf
mermaidseaweav <eng.gameliaahmed@gmail.com>
2023-02-21 02:46:43 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphbet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void times_table(void);
+void jack_bauer(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
