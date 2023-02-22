d6f5f4e22c476104076ac7fb7d8ad9802acf6f08
mermaidseaweav <eng.gameliaahmed@gmail.com>
2023-02-22 09:16:24 -0800

    Update 2-main.h

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
 * largest_number - returns the largest of 3 numbers
 * @a: first integer
--- 0x03-debugging/2-main.h
+++ 0x03-debugging/2-main.h
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
 * main - prints the largest of 3 integers
 * Return: 0
