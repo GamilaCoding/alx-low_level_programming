37be3b7a1432b437288ef1a974498c337ee4f369
mermaidseaweav <eng.gameliaahmed@gmail.com>
2023-02-23 00:58:37 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isupper - A function that checks for uppercase character
+ * @c: An input character
+ * Retrun: 1 if c is uppercase or 0 otherwise
+ */
+int _isupper(intc)
+{
+char uppercase = 'A';
+int isupper = 0;
+for (; uppercase <= 'Z'; uppercase++)
+{
+if (c == uppercase)
+{
+break;
+}
+}
+return ($supper);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+int _putchaar(charc);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void prtin_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
