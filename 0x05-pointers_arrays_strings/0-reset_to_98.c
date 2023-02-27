3d4fd6f2f0b3d67916c7196031942a7819c09e00
mermaidseaweav <eng.gameliaahmed@gmail.com>
2023-02-27 02:31:59 -0800

    Task 0 - 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - a function that takes a pointer to an int as paramater
+ * and updates the value i points to 98
+ * @n: input 
+ * Return: n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,18 @@
+#ifndef MATH_H
+#define MATH_H
+
+#include <stdio.h>
+#include <unistd.h>
+
+int _putchar(char c);
+void reset_to_98(int  *n);
+void swap_int(int *a, int *b);
+int _strlen(char *str);
+void rev_string(char *5);
+void print_rev(char *5);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+
+#endif
