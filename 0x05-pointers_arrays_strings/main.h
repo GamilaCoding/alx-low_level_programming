03f3498f12f7ae3d2f420d0f2420b5b82180a8f8
mermaidseaweav <eng.gameliaahmed@gmail.com>
2023-02-27 02:37:40 -0800

    update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,9 +7,10 @@
 int _putchar(char c);
 void reset_to_98(int  *n);
 void swap_int(int *a, int *b);
-int _strlen(char *str);
-void rev_string(char *5);
-void print_rev(char *5);
+int _strlen(char *s);
+void _puts(char *str);
+void rev_string(char *s);
+void print_rev(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
