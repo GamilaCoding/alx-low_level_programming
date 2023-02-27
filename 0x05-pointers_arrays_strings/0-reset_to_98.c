22dbc89ffdaf808769c9e01ce12f2dbf973f2e3a
mermaidseaweav <eng.gameliaahmed@gmail.com>
2023-02-27 02:51:00 -0800

    0-reset_to_98.c-update

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * reset_to_98 - takes a pointer to an int
- * @n: take an int 
- * Return: int
- */
+* reset_to_98 -  takes a pointer to an int
+* @n: take an int
+* Return: int
+**/
 
 void reset_to_98(int *n)
 {
