08d7c2f0b26aab22e2db14608f0d41e1a1584c97
mermaidseaweav <eng.gameliaahmed@gmail.com>
2023-03-20 04:26:55 -0700

    update new of file 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-/*
- * main - prints the name of the file program was complier from
- * Return: Alawys 0
- */
-
+/**
+* main - prints the name of the file it was compiled from
+*
+* Return: always 0
+**/
 int main(void)
 {
-printf("%s\n", __FILE__);
-retrun (0);
+	printf("%s\n", __FILE__);
+	return (0);
 }
