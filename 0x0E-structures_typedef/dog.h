f176830202f471ad7760609c2e5b675551e87342
mermaidseaweav <eng.gameliaahmed@gmail.com>
2023-03-20 13:08:43 -0700

    add dog.h file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,25 +1,22 @@
 #ifndef DOG_H
 #define DOG_H
-
 /**
- * struct dog - Struct dog
- * @name: name
- * @age: age
- * @owner: owner
- * Description: This struct is for dog
- */
-
-struct dog
+* struct dog - dog data
+* @name: name of dog
+* @age: age of dog
+* @owner: name of owner dogs
+* Description: Defines metadata about the dog and type of dogs
+**/
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-
-typedef struct dog dog_t;
-void init_dog(struct dog, *d, char *name, float age, char *owner);
+} dog_t;
+/* Additional function prototypes */
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-
-#endif /*DOG_H*/
+#endif
