f1a55674cd8d963c43ee921b1c4c80f43e379a17
mermaidseaweav <eng.gameliaahmed@gmail.com>
2023-03-22 06:45:22 -0700

    done 0-print_name.c, main file , and funcation.h

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,12 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+#include <stddef.h>
+#include <stdlib.h>
+
+void print_name(char *name, void (*f)(char *));
+int _putchar(char c);
+void arrary_interator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
