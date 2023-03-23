0ecaebe17e3d80c3dcd6b5fb270ed6a5d40eab06
mermaidseaweav <eng.gameliaahmed@gmail.com>
2023-03-23 10:40:42 -0700

    Done files variadic_functions.h, 0-main.c, and 0-sum_them_all.c

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,11 @@
+#ifndef VARIADIC_FUNCTION_H
+#define VARIADIC_FUNCTION_H
+
+#include <stdarg.h>
+#include <stdio.h>
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+#endif
