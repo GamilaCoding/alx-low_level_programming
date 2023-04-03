b2d6e35837850f89d5b0e92f9ef2ee4f58aeb7cc
mermaidseaweav <eng.gameliaahmed@gmail.com>
2023-04-03 14:36:24 -0700

    Submit some files 0-print_listint.c, and 0-main.c; including Header file

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_listint - a function of all elements print list
+ * @h: holds the head to linked list
+ * Return: The number of the nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+size_t node_C;
+const listint_t *next_ptr;
+next_ptr = h;
+node_C = 0;
+while (next_ptr != NULL)
+{
+printf("%i\n", next_ptr->n);
+node_C += 1;
+next_ptr = next_ptr->next;
+}
+return (node_C);
+}
---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,23 @@
+#define LISTS_H_H
+#ifdef LISTS_H_H
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * 
+ */
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+void free_listint(listint_t *head);
+int pop_listint(listint_t **head);
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+#endif
