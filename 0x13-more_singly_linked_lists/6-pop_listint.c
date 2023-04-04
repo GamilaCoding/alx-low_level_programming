89ef97e3fa69a3c61c79cf3cb9d2434bf6592f42
mermaidseaweav <eng.gameliaahmed@gmail.com>
2023-04-04 12:48:18 -0700

    Done 6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -5,22 +5,22 @@
 /**
 *pop_listint - deletes the head node of a linked list
 *@head: head of the list
-*
-*Return: the deleted node data
+*Return: the deleted node data(n)
 */
 
 int pop_listint(listint_t **head)
 {
-listint_t *popped;
-int content;
+int data_n;
+listint_t *pop_node;
 
+if (head == NULL)
+	return (0);
 if (*head == NULL)
-return (0);
-content = 0;
-popped = *head;
-content = (*listint_t)->n;
-
+	return (0);
+data_n = 0;
+pop_node = *head;
+data_n = (*head)->n;
 *head = (*head)->next;
-free(popped);
-return (content);
+free(pop_node);
+return (data_n);
 }
