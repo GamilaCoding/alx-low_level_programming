12303e164a9a5e382c8921e82363e582e1459277
mermaidseaweav <eng.gameliaahmed@gmail.com>
2023-04-04 15:00:41 -0700

    Done Task 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -3,12 +3,13 @@
 #include <stdio.h>
 #include "lists.h"
 /**
-* insert_nodeint_at_index - afunc that inserts a new node at a given position
-* @head: a pointer to pointer to struct
-* @idx: is the index of the list where the new node 
-* should be added index starts at 0
-* @n: the data for the new node
-* Return: the address of the new node, or NULL if it failed
+*insert_nodeint_at_index - a function that inserts a new
+*node at a given position
+*@head: a pointer to pointer to struct
+*@idx: is the index of the list where the new node 
+*should be added index starts at 0
+*@n: the data for the new node
+*Return: the address of the new node, or NULL if it failed
 */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
