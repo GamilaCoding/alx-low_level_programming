62468a0053cf0a990e8e3ad08beccfae0ba82e6f
mermaidseaweav <eng.gameliaahmed@gmail.com>
2023-04-04 06:22:55 -0700

    Done 9-insert_nodeint.c

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * insert_nodeint_at_index - afunc that inserts a new node at a given position
+ * @head: a pointer to pointer to struct
+ * @idx: is the index of the list where the new node shiuld be added index starts at 0
+ * @n: the data for the new node
+ * Return: the address of the new node, or NULL if it failed
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+listint_t *next_ptr;
+listint_t *previous_ptr;
+listin_t *new_node;
+unsigned int i;
+
+if (head == NULL)
+return (NULL);
+next_ptr = *head;
+previous_ptr = NULL;
+for (i = 0; i < idx; i += 1)
+{
+if (next_ptr == NULL)
+return (NULL);
+previous_ptr = next_ptr;
+next_ptr = next_ptr->next;
+}
+new_node = malloc(sizeof(listint_t));
+if (!new_node)
+return (NULL)
+new_node->n = n;
+new_node->next = next_ptr;
+if (idx == 0)
+*head = new_node;
+else
+previous_ptr->next = new_node;
+return (new_node);
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,12 +1,11 @@
-#define LISTS_H
-#ifdef LISTS_H
-#include <stddef.h>
-/**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
+#define "LISTS_H_H"
+#ifndef "LISTS_H_H"
+/*
+ * struct_listint_s - signly linked list
+ * @n: integar
+ * @next: point to the next node
  *
- * Description: singly linked list node structure
+ * Descriptions: signly linked list node structure
  * for ALX project
  */
 typedef struct listint_s
@@ -14,18 +13,18 @@ typedef struct listint_s
     int n;
     struct listint_s *next;
 } listint_t;
-
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
-void free_listint(listint_t *head);
-int pop_listint(listint_t **head);
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 listint_t *add_nodeint_end(listint_t **head, const int n);
-int sum_listint(listint_t *head);
-int delete_nodeint_at_index(listint_t **head, unsigned int idx, int n);
+void free_listint(listint_t *head);
 void free_listint2(listint_t **head);
-size_t free_listint_t_safe(listint_t **h);
+int pop_listint(listint_t **head);
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
+list_t *reverse_listint(listint **head);
 size_t print_listint_safe(const listint_t *head);
-listint_t *insert_nodeint_t_index(listint_t **head, unsigned int idx, int n);
+size_t free_listint_loop(listint_t **h);
+listint_t *find_listint_loop(listint_t *head);
 #endif
