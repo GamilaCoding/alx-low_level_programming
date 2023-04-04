856f9a29a6446710cc4ebbc2ef1079440c921cd1
mermaidseaweav <eng.gameliaahmed@gmail.com>
2023-04-04 12:59:29 -0700

    Done 8-sum_listint.c

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -3,21 +3,22 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- * sum_listint - sum up  all the date in linked list
- * @head: head of the list
- *
- * Return: sum of thenumber
+ *sum_listint - a func that returns the sum of all the data
+ *@head: head of the list
+ *Return: sum of thenumber
  */
 
 int sum_listint(listint_t *head)
 {
-listint_t *next_ptr;
 int sum;
+listint_t *next_ptr;
+
 if (!head)
 return (0);
 next_ptr = head;
 sum = 0;
 while (next_ptr != NULL)
+{
 sum += next_ptr->n;
 next_ptr = next_ptr->next;
 }
