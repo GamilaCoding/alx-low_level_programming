093b5e28be6eecf9714c15393c20c30af7ddaf27
mermaidseaweav <eng.gameliaahmed@gmail.com>
2023-04-04 12:23:32 -0700

    3-add_nodeint_end.c Done

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 #include <stdlib.h>
 #include <string.h>
-#include <stdlib.h>
+#include <stdio.h>
 /**
 *pop_listint - deletes the head node of a linked list
 *@head: head of the list
@@ -18,7 +18,7 @@ if (*head == NULL)
 return (0);
 content = 0;
 popped = *head;
-content = (*popped)->n;
+content = (*listint_t)->n;
 
 *head = (*head)->next;
 free(popped);
