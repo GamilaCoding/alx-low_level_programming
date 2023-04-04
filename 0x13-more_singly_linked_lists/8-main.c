ce155dd4e72ac3d6361f722a94a6c6cb1f58abd1
mermaidseaweav <eng.gameliaahmed@gmail.com>
2023-04-03 18:37:30 -0700

    Done 8-sum_listint.c

--- 0x13-more_singly_linked_lists/8-main.c
+++ 0x13-more_singly_linked_lists/8-main.c
@@ -2,7 +2,6 @@
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
-
 /**
  * main - check the code
  *
@@ -10,20 +9,19 @@
  */
 int main(void)
 {
-    listint_t *head;
-    int sum;
-
-    head = NULL;
-    add_nodeint_end(&head, 0);
-    add_nodeint_end(&head, 1);
-    add_nodeint_end(&head, 2);
-    add_nodeint_end(&head, 3);
-    add_nodeint_end(&head, 4);
-    add_nodeint_end(&head, 98);
-    add_nodeint_end(&head, 402);
-    add_nodeint_end(&head, 1024);
-    sum = sum_listint(head);
-    printf("sum = %d\n", sum);
-    free_listint2(&head);
+listint_t *head;
+int sum;
+head = NULL;
+add_nodeint_end(&head, 0);
+add_nodeint_end(&head, 1);
+add_nodeint_end(&head, 2);
+add_nodeint_end(&head, 3);
+add_nodeint_end(&head, 4);
+add_nodeint_end(&head, 98);
+add_nodeint_end(&head, 402);
+add_nodeint_end(&head, 1024);
+sum = sum_listint(head);
+printf("sum = %d\n", sum);
+free_listint2(&head);
 return (0);
 }
