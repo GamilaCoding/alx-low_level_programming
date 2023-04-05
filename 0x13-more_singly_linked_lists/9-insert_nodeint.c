be93bf2353b064763b0e6b6c7bb460b7c8e07897
mermaidseaweav <eng.gameliaahmed@gmail.com>
2023-04-04 20:01:00 -0700

    Update task nine

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -2,38 +2,37 @@
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
+
 /**
-*insert_nodeint_at_index - a function that inserts a new
-*node at a given position
-*@head: a pointer to pointer to struct
-*@idx: is the index of the list where the new node 
-*should be added index starts at 0
-*@n: the data for the new node
-*Return: the address of the new node, or NULL if it failed
+*insert_nodeint_at_index - a function that insert 
+*a new node at given position
+*@head: a pionter to pointer to struct 
+*@idx: the of index the new list where the new node
+*should be added index starts 0
+*@n:the data for new node
+*Return: the address for new node, or NULL if it failed
 */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 listint_t *next_ptr;
+listint_t *new_node;
 listint_t *previous_ptr;
-listin_t *new_node;
-unsigned int i;
-
+unsigned int n;
 if (head == NULL)
 return (NULL);
 next_ptr = *head;
 previous_ptr = NULL;
-for (i = 0; i < idx; i += 1)
+for (i = 0 ; i < idx; i += 1)
 {
 if (next_ptr == NULL)
 return (NULL);
-previous_ptr = next_ptr;
+pervious_ptr = next_ptr;
 next_ptr = next_ptr->next;
 }
 new_node = malloc(sizeof(listint_t));
 if (!new_node)
-return (NULL)
+retrurn (NULL);
 new_node->n = n;
-new_node->next = next_ptr;
 if (idx == 0)
 *head = new_node;
 else
