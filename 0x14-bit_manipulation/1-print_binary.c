ebd8fd4714ca9e316e2931007f2b67b781f74100
mermaidseaweav <eng.gameliaahmed@gmail.com>
2023-04-07 12:16:34 -0700

    Add some decleration functions and file 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,19 +1,41 @@
-#include <stdio.h>
 #include "main.h"
 /**
-*print_binary - a function that prints the binary
-*representation of number
-*@n: long integer
-*Return: nothing
+*_power - calculate (base and power)
+*@base: base of exponet
+*@pow: power of the exponet
+*Return: value of base and power
+*/
+unsigned long int _power(unsigned int base, unsigned int pow)
+{
+unsigned long int num;
+unsigned int i;
+num = 1;
+for (i = 1; i <= pow; i++)
+num *= base;
+return (num);
+}
+/**
+*print_binary - prints the binary representation of number
+*@n: num of prented
+*Return: void
 **/
 void print_binary(unsigned long int n)
 {
-if ((n >> 1) == 0 && n == 0)
+unsigned long int dev, result;
+char flag;
+flag = 0;
+dev = _power(2, sizeof(unsigned long int) * 8 - 1);
+while (dev != 0)
+{
+result = n & dev;
+if (result == dev)
+{
+flag = 1;
+_putchar('1');
+}
+else if (flag == 1 || dev == 1)
 {
 _putchar('0');
-return;
 }
-if (n >> 1 != 0)
-print_binary(n >> 1);
-_putchar((n & 1) + '0');
+dev >>= 1;
 }
