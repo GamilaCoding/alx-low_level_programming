f2cffdc5e9f78fd72c57e3a7a7a8053795951fab
mermaidseaweav <eng.gameliaahmed@gmail.com>
2023-04-08 20:02:34 -0700

    Edited some files

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,22 +1,31 @@
 #include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
 /**
-*main - check the code 
-*
-*Return: Alawys 0.
-**/
-int main(int ac, char **av)
+ * read_textfile - reads a text file and prints it to posx stdout
+ * @filename: pointer to the name of the file.
+ * @letters: the number of letters the function
+ *should read and print.
+ * Return:if function fails or filename is NULL - 0.
+ *O/W- the actual number of types the function can read and print.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
 {
-ssize_t n;
-if (ac != 2)
-{
-dprintf(2, "Usage: %s filename\n", av[0]);
-exit(1);
-}
-n = read_textfile(av[1], 114);
-printf('\n(printed chars: %li)"\n, n);
-n = read_textfile(av[1], 1024);
-printf("\n(printed chars: %li)\n", n);
+ssize_t o, r, w;
+char *buffer;
+if (filename == NULL)
 return (0);
+buffer = malloc(sizeof(char) * letters);
+if (buffer == NULL)
+return (0);
+o = open(filename, O_RDONLY);
+r = read(o, buffer, letters);
+w = write(STDOUT_FILENO, buffer, r);
+if (o == -1 || r == -1 || w == -1 || w != r)
+{
+free(buffer);
+return (0);
+}
+free(buffer);
+close(o);
+return (w);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,14 +1,15 @@
-#ifdef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
-#include <stdlib.h>
+
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <unistd.h>
-#include <string.h>
-#include <stdio.h>
+#include <stdlib.h>
+
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int _putchar(char c);
-#endif
+
+#endif /*MAIN_H*/
