185ec26c9d0cb678e5ebd0a484a699854157fe43
gameliaprogrammer <eng.gameliaahmed@gmail.com>
2023-10-12 05:01:42 -0700

    header file

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,31 +1,30 @@
 #include "lists.h"
-#include <stdio.h>
 
 /**
- * print_dlisint - print all the elements of a
- * dlistint_t list
- * @h: head of list
- * Return: number of nodes
- */
+* print_dlistint - print a double linked list
+* @h: header of double linked list
+* Return: number of nodes
+*/
 size_t print_dlistint(const dlistint_t *h)
 {
-size_t i = 0;
-int iniprint = 0;
+	size_t i = 0;
+	int iniprint = 0;
 
-for (i = 0; h != NULL; i++)
-{
-if (h->prev != NULL && iniprint == 0)
-{
-h = h->prev;
-continue;
-}
-else if (h->prev == NULL)
-{
-iniprint = 1;
-i = 0;
-}
-printf("%i\n", h->n);
-h = h->next;
-}
-return (i);
+	for (i = 0; h != NULL; i++)
+	{
+		if (h->prev != NULL && iniprint == 0)
+		{
+			h = h->prev;
+			continue;
+		}
+		else if (h->prev == NULL)
+		{
+			iniprint = 1;
+			i = 0;
+		}
+		printf("%i\n", h->n);
+		h = h->next;
+
+	}
+	return (i);
 }
